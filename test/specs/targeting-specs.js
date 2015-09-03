a7cf65c2de543618d6776d85d5501889a4d2052c
Dan Gilkerson <dan.gilkerson@versionone.com>
2015-09-03 12:18:39 -0400

    Load test webpages from file

--- test/specs/targeting-specs.js
+++ test/specs/targeting-specs.js
@@ -1,7 +1,9 @@
 describe('Targeting', function () {
     it('will do something', function* () {
-        yield browser.url('http://google.com');
+        yield browser.url("file:///" + __dirname + "/examples/labels.html");
+        //yield browser.url('http://google.com');
+
         var title = yield browser.getTitle()
-        console.log(title);
+        console.log("Title:", title);
     });
 });
