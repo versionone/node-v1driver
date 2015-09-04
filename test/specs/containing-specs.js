a4460357057f8a006d3f8e51164d5866d75ccaff
Dan Gilkerson <dan.gilkerson@versionone.com>
2015-09-04 14:57:06 -0400

    Error on duplicate find

--- test/specs/containing-specs.js
+++ test/specs/containing-specs.js
@@ -12,4 +12,12 @@ describe('Containing', function () {
 		var content = yield browser.getHTML("Item 1 in box 3>Item 2")
 		content.should.equal('<div class="box3-item-2">Item 2</div>');
 	});
+
+	it("should traverse the dom looking for items in parent containers", function*() {
+		yield browser.url("file:///" + __dirname + "/examples/containers.html")
+
+		yield browser.getHTML("box4>Duplicate A").catch(function(err){
+			err.message.should.equal("Found 2 duplicates for: box4>Duplicate A")
+		})
+	});
 });
