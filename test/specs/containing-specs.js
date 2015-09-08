654ef2011e217e943166a73efd8e4073859bac8e
Dan Gilkerson <dan.gilkerson@versionone.com>
2015-09-08 14:03:37 -0400

    Nth specs

--- test/specs/containing-specs.js
+++ test/specs/containing-specs.js
@@ -1,30 +1,31 @@
 describe('Containing', function () {
-	it("should look inside a container", function*() {
+	before(function*() {
 		yield browser.url("file:///" + __dirname + "/examples/containers.html")
+	});
 
+	it("should look inside a container", function*() {
 		var content = yield browser.getHTML("box2>Item 1")
 		content.should.equal('<div class="box2-item">Item 1</div>');
 	});
 
 	it("should traverse the dom looking for items in parent containers", function*() {
-		yield browser.url("file:///" + __dirname + "/examples/containers.html")
-
 		var content = yield browser.getHTML("Item 1 in box 3>Item 2")
 		content.should.equal('<div class="box3-item-2">Item 2</div>');
 	});
 
 	it("should traverse the dom looking for items in parent containers", function*() {
-		yield browser.url("file:///" + __dirname + "/examples/containers.html")
-
 		yield browser.getHTML("box4>Duplicate A").catch(function(err){
 			err.message.should.equal("Found 2 duplicates for: box4>Duplicate A")
 		})
 	});
 
 	it("should traverse the dom looking for items in parent containers", function*() {
-		yield browser.url("file:///" + __dirname + "/examples/containers.html")
-
 		var content = yield browser.getHTML("box5>inner-box>Item 1");
+		content.should.equal('<div class="box5-item-1">Item 1</div>');
+	});
+
+	it("should search down only", function*() {
+		var content = yield browser.getHTML("box5-item-2>Item 1");
 		content.should.equal('<div class="box5-item-1">Item 1</div>');
 	});
 });
--- test/specs/targeting-specs.js
+++ test/specs/targeting-specs.js
@@ -1,28 +1,24 @@
 describe('Targeting', function () {
-	it("should look by content", function*() {
+	before(function*() {
 		yield browser.url("file:///" + __dirname + "/examples/labels.html")
+	})
 
+	it("should look by content", function*() {
 		var content = yield browser.getHTML("Content Item")
 		content.should.equal('<div>Content Item</div>');
 	});
 
 	it('will look by id', function* () {
-		yield browser.url("file:///" + __dirname + "/examples/labels.html")
-
 		var content = yield browser.getHTML("label-id")
 		content.should.equal('<div id="label-id">ID Item</div>');
 	});
 
 	it("should look by class", function*() {
-		yield browser.url("file:///" + __dirname + "/examples/labels.html")
-
 		var content = yield browser.getHTML("div-class")
 		content.should.equal('<div class="div-class">Class Item</div>');
 	});
 
 	it.skip("should look at attributes by value", function*() {
-		yield browser.url("file:///" + __dirname + "/examples/labels.html")
-
 		var content = yield browser.getHTML("attribute-value")
 		content.should.equal('<div data-key="attribute-value">Attribute Item</div>');
 	});
