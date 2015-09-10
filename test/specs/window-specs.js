51d12a0ffeaa48bca3547cf9369c13f4464a3d2a
Dan Gilkerson <dan.gilkerson@versionone.com>
2015-09-09 18:40:31 -0700

    Popup tests

---
+++ test/specs/window-specs.js
@@ -0,0 +1,36 @@
+describe('Window', function () {
+    it.only("should change focus to popup window", function*() {
+        browser.addCommand("activateNewWindow", function () {
+            return this.pause(1000).getTabIds().then(function (handles) {
+                return this.getCurrentTabId().then(function (current) {
+                    if (handles[0] == current)
+                        return this.window(handles[1])
+                    else
+                        return this.window(handles[0])
+                })
+            })
+        });
+
+        browser.addCommand("activateOnlyWindow", function () {
+
+            return this.pause(1000).getTabIds()
+                .then(function (handles) {
+                    //console.log(handles);
+                    return this.window(handles[0])
+                })
+        });
+
+
+        yield browser.url("file:///" + __dirname + "/examples/window.html");
+        yield browser.click("Popup");
+        yield browser.activateNewWindow();
+        var content = yield browser.getHTML("Popup Window")
+        content.should.equal('<div>Popup Window</div>');
+
+        yield browser.click("Close")
+        yield browser.activateOnlyWindow();
+
+        var content = yield browser.getHTML("Popup")
+        content.should.equal('<a href="./new-window.html" target="_blank">Popup</a>');
+    });
+});
